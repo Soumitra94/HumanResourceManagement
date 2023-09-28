51d3b2b29645f1bc3e111cf2908a97fece42cf14
ASUS <ASUS@LAPTOP-D1JRC1ED>
2023-09-28 11:08:43 +0530

    commit 105

---
+++ HumanResourceManagement/src/test/java/HrHeadModuleTest/NewClass.java
@@ -0,0 +1,13 @@
+package HrHeadModuleTest;
+
+import org.testng.annotations.Test;
+
+public class NewClass
+{
+	@Test
+	public void newClass()
+	{
+		System.out.println("New Class Created");
+	}
+
+}
